80168dec0dd8a89675cf354121ccea72ca0b3a3a
Gaaraly <1205913452@qq.com>
2021-03-21 21:02:33 +0800

    尝试支持渠道服

--- skyaddfriends.js
+++ skyaddfriends.js
@@ -1,34 +1,43 @@
 "auto";
-var url = rawInput('请输入好友链接');
+var url = rawInput("请输入好友链接");
 url = url.trim();
 //校验链接
 var isNetease = /^http[s]{0,1}:\/\/sky.163.com\/yqlj\/\?i=[\w_-]+$/.test(url);
 var isInternational = /^http[s]{0,1}:\/\/sky.thatg.co\/\?i=[\w_-]+$/.test(url);
 var isTest = /^http[s]{0,1}:\/\/sky-beta.thatg.co\/\?i=[\w_-]+$/.test(url);
-var packageName,className;
+var packageName, className;
+var getPackagename;
 //判断国服或国际服链接
-if (isNetease == false && isInternational == false  && isTest == false) {
-    toast('请检查链接是否正确！');
+if (isNetease == false && isInternational == false && isTest == false) {
+	toast("请检查链接是否正确！");
 } else if (isNetease) {
-    toast('添加国服好友！');
-    packageName = 'com.netease.sky';
-    className = 'com.tgc.sky.netease.GameActivity_Netease';
+	toast("添加国服好友！");
+	packageName = "com.netease.sky";
+	className = "com.tgc.sky.netease.GameActivity_Netease";
 } else if (isInternational) {
-    toast('添加国际服好友！');
-    packageName = 'com.tgc.sky.android';
-    className = 'com.tgc.sky.GameActivity';
-} else if (isTest){
-    toast('添加测试际服好友！');
-    packageName = 'com.tgc.sky.android.test.gold';
-    className = 'com.tgc.sky.GameActivity';
+	toast("添加国际服好友！");
+	packageName = "com.tgc.sky.android";
+	className = "com.tgc.sky.GameActivity";
+} else if (isTest) {
+	toast("添加测试际服好友！");
+	packageName = "com.tgc.sky.android.test.gold";
+	className = "com.tgc.sky.GameActivity";
 }
-log(packageName, '=>', url);
+//判断渠道服版本
+if (isNetease) {
+	getPackagename = currentPackage();
+	if (getAppName(getPackagename) == "光·遇") {
+		packageName = getPackagename;
+	}
+}
+
+log(packageName, "=>", url);
 //启动app
 if (isNetease || isInternational || isTest) {
-    app.startActivity({
-        action: "android.intent.action.VIEW",
-        data: url,
-        packageName: packageName,
-        className: className
-    });
+	app.startActivity({
+		action: "android.intent.action.VIEW",
+		data: url,
+		packageName: packageName,
+		className: className,
+	});
 }
